355de6131ccd7cb48dd0871b16b25f276b08050d
John Hawthorn <john@hawthorn.email>
2018-06-19 17:59:24 -0700

    Post every 2m that I drink the beer

--- handler.js
+++ handler.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const { WebClient } = require('@slack/client');
+
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+
+const web = new WebClient(SLACK_TOKEN);
+const conversationId = process.env.SLACK_CONVERSATION_ID;
+
+const postMessageToSlack = (message) => {
+  web.chat.postMessage({ channel: conversationId, text: message })
+    .then((res) => {
+      // `res` contains information about the posted message
+      console.log('Message sent: ', res.ts);
+    })
+    .catch(console.error);
+};
+
+
 module.exports.helloWorld = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -14,3 +31,7 @@ module.exports.helloWorld = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.pollForBeers = (event, context) => {
+  postMessageToSlack("jhawthorn is drinking the beer");
+}
