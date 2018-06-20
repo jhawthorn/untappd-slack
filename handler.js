412065fd7175a31e2312728ad3c7f11cc0e19b13
John Hawthorn <john@hawthorn.email>
2018-06-19 18:19:51 -0700

    Remove hello, world

--- handler.js
+++ handler.js
@@ -19,22 +19,6 @@ const postMessageToSlack = (message) => {
     .catch(console.error);
 };
 
-
-module.exports.helloWorld = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
-
 module.exports.pollForBeers = (event, context) => {
   postMessageToSlack("jhawthorn is drinking the beer");
 }
